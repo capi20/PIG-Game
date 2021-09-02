d8d6143ebf8e91d883d4a0161bb289f01aab749a
capi20 <shyam20998@gmail.com>
2021-09-02 18:06:20 +0530

    create seperate folder for images

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 	var diceDOM = document.querySelector('.dice')
 	diceDOM.style.display = 'block';
-	diceDOM.src = 'dice-' + dice + '.png';
+	diceDOM.src = './images/dice-' + dice + '.png';
 
 	if (dice !== 1){
 		roundScore += dice;
